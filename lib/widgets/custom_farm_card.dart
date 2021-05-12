db5ed5338f70413fbac1769de807fe922da517c0
Vivek <vivekkoparde3624@gmail.com>
2021-05-12 13:15:39 +0530

    Made changes in profile screen

--- lib/widgets/custom_farm_card.dart
+++ lib/widgets/custom_farm_card.dart
@@ -6,54 +6,90 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 class FarmCard extends StatelessWidget {
   final Farm farm;
   Function fun;
-  FarmCard({this.farm,this.fun});
+  FarmCard({this.farm, this.fun});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: GestureDetector(
-        onTap: (){selectFarm(farm.id,context);},
+        onTap: () {
+          selectFarm(farm.id, context);
+        },
         child: Card(
-          color: farm.isSelected?Colors.green:Colors.white,
-          child: Column(
-            children: [
-              Text('Farm Name : ' + farm.farmName),
-              Text('Longitude : ' + farm.longitude.toString()),
-              Text('Latitude : ' + farm.latitude.toString()),
-              Text('Area : ' + farm.area.toString()),
-              Text('Is Selected : '+farm.isSelected.toString()),
-            ],
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          color: farm.isSelected ? Colors.green : Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 40.0,top: 10.0,bottom: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Farm Name : ' + farm.farmName,
+                  style: farm.isSelected
+                      ? TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold)
+                      : TextStyle(color: Colors.green),
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text('Longitude : ' + farm.longitude.toString(),
+                            style: farm.isSelected
+                                ? TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold)
+                                : TextStyle(color: Colors.green)),
+                        Text('Latitude : ' + farm.latitude.toString(),
+                            style: farm.isSelected
+                                ? TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold)
+                                : TextStyle(color: Colors.green)),
+                        Text('Area : ' + farm.area.toString(),
+                            style: farm.isSelected
+                                ? TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold)
+                                : TextStyle(color: Colors.green)),
+                        Text('Is Selected : ' + farm.isSelected.toString(),
+                            style: farm.isSelected
+                                ? TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold)
+                                : TextStyle(color: Colors.green)),
+                      ],
+                    ),
+                    SizedBox(width: 30.0,),
+                    Image.asset('images/Plot.png',height:90.0,width: 100.0,)
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  selectFarm(String farmId,BuildContext context) async{
+  selectFarm(String farmId, BuildContext context) async {
     showDialog(
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
           return Center(
             child: SpinKitDoubleBounce(
-                itemBuilder:
-                    (BuildContext context,
-                    int index) {
-                  return DecoratedBox(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: index.isEven
-                          ? Colors.white
-                          : Color(0xff00A961),
-                    ),
-                  );
-                }),
+                itemBuilder: (BuildContext context, int index) {
+              return DecoratedBox(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: index.isEven ? Colors.white : Color(0xff00A961),
+                ),
+              );
+            }),
           );
         });
     String selectedFarmId = await getSelectedFarmId();
-    await changeFarmSelectedStatus(selectedFarmId,"false");
-    await changeFarmSelectedStatus(farmId,"true");
-    fun((){
+    await changeFarmSelectedStatus(selectedFarmId, "false");
+    await changeFarmSelectedStatus(farmId, "true");
+    fun(() {
       print("SET STAET");
     });
     Navigator.pop(context);
